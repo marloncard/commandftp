48bf66f61905a5bf0b993efb71c38c2a59ac5989
Marlon Card <marloncard@gmail.com>
2015-02-19 21:43:20 -0500

    Started using git (no changes yet)

---
+++ ftpclient.py
@@ -0,0 +1,76 @@
+from ftplib import FTP
+# Add better error handling especially for getfile(trying to get it to work) **REVIEW TREEHOUSE LESSON**
+# Add delete remote file (rm)
+# Add move file (mv)
+# Add ability to change file permissions?
+
+user = 'qualico@qualicotrading.com'
+passw = '987!2Hpu8$9'
+url = 'ftp.qualicotrading.com'
+ftp = FTP(url)		#connect to the host, default port
+
+def connect():
+	ftp.login(user, passw) # enters login info
+	ftp.dir				# view current directory listing
+	menu()
+
+def disconnect():
+	really = input('Really end session? ').lower().strip()
+	if really == 'y':
+		ftp.quit()
+	else:
+		menu()
+		
+def getfile():
+	filename = input('Name of file: ')
+	localfile = open(filename, 'wb')
+	ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+	localfile.close()
+	menu()
+	
+def putfile():
+	filename = input('Name of file: ')
+	ftp.storbinary('STOR ' + filename, open(filename, 'rb'))
+	menu()
+	
+	
+def pwd(): #Sort of works...
+	print('')
+	print(ftp.pwd())
+	menu()
+	
+def viewdir():
+	ftp.retrlines('LIST') 	# List directory contents
+	menu()
+	
+def cwd():
+	ftp.cwd(input('path: ')) # Change current directory.
+	menu()
+	
+def menu():
+	print('')
+	print('*' * 10)
+	print('[q] to quit')
+	print('[ls] to list dir contents')
+	print('[pwd] to show working dir')
+	print('[cd] to change dir')
+	print('[get] to download file')
+	print('[put] to send file')
+	
+	command = input('What is your command: ').lower().strip()
+	if command == 'q':
+		disconnect()
+	elif command == 'ls':
+		viewdir()
+	elif command == 'pwd':
+		pwd()
+	elif command == 'cd':
+		cwd()
+	elif command == 'get':
+		getfile()
+	elif command == 'put':
+		putfile()
+	else:
+		menu()
+
+connect()
